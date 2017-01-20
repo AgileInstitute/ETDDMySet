09a7e453a23ff6c41f922c8762eb60c4766f8c13
Rob Myers <Rob.Myers@agileInstitute.com>
2017-01-19 19:19:59 -0800

    moved a file

--- src/tests/MySetTests.java
+++ src/tests/MySetTests.java
@@ -1,10 +1,9 @@
-package myset.tests;
+package tests;
 
 import myset.Set;
 
 import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
