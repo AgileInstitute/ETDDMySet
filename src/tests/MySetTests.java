6e2eb63c318f59dc6faebb5a0ba5e6f4907b0c66
Rob Myers <Rob.Myers@agileInstitute.com>
2017-01-21 18:00:49 -0800

    more refactoring for the book

--- src/tests/MySetTests.java
+++ src/tests/MySetTests.java
@@ -8,27 +8,26 @@ import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MySetTests {
-    public static final String ITEM_TO_FIND = "Not the 1st item and contains a typ0";
+    public static final String ITEM_TO_FIND =
+            "Not the 1st item and contains a typ0";
     Set set;
 
     @BeforeEach
     public void initializeSet() {
-        //given
-        set = new Set(new String[] { "some uninteresting item" , ITEM_TO_FIND });
+        set = new Set(new String[] {
+                "some uninteresting item",
+                ITEM_TO_FIND });
     }
 
     @Test
     public void doesNotContainAnElement() {
-        // when/then
-        assertThat(set.contains("item not in the set"), is(false));
+        assertThat(set.contains("not an element"), is(false));
     }
 
-    @Test @DisplayName("contains an element (constant avoids test typos)")
+    @Test
     public void containsAnElement() {
-        // when/then
         assertThat(set.contains(ITEM_TO_FIND), is(true));
     }
 
@@ -42,5 +41,4 @@ public class MySetTests {
         set = new Set();
         assertThat(set.isEmpty(), is(true));
     }
-
 }
