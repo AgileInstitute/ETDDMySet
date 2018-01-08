72dae9433985fad1e14aa7a7b01f7c2bbdc4ae81
Rob Myers <Rob.Myers@agileInstitute.com>
2018-01-08 12:29:11 -0800

    Hamcrest containsString ==> AssertJ contains
    …and I updated the IDE, which broke everything until I re-imported
    JUnit5. At least now I’m not running a beta of JUnit5! :-]

--- src/tests/MySetExceptionTests.java
+++ src/tests/MySetExceptionTests.java
@@ -3,22 +3,19 @@ package tests;
 import myset.InvalidElementException;
 import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.CoreMatchers.containsString;
+import static org.assertj.core.api.Java6Assertions.*;
 
 public class MySetExceptionTests {
     @Test
     public void messageFormatsNullReference() {
         InvalidElementException myException =
                 new InvalidElementException(null);
-        assertThat(myException.getMessage(),
-                containsString("[null]"));
+        assertThat(myException.getMessage()).contains("[null]");
     }
     @Test
     public void messageContainsTheInvalidValue() {
         InvalidElementException myException =
                 new InvalidElementException("bleep!");
-        assertThat(myException.getMessage(),
-                containsString("[\"bleep!\"]"));
+        assertThat(myException.getMessage()).contains("[\"bleep!\"]");
     }
 }
