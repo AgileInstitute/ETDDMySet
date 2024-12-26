5402ed6087a2db6631c231371647f26c92619378
Rob Myers <Rob.Myers@agileInstitute.com>
2024-12-26 14:26:49 -0800

    Elements are now IDs; I prefer String.format() over concatenation.

--- src/crittermaps/InvalidIDException.java
+++ src/crittermaps/InvalidIDException.java
@@ -1,12 +1,10 @@
 package crittermaps;
-
 public class InvalidIDException extends RuntimeException {
     public InvalidIDException(String badID)
     {
-        super("Invalid set element value [" +
-                (badID != null ?
-                        String.format("\"%s\"", badID) :
-                        badID) +
-                "]");
+        super(String.format("Invalid tracker ID value [%s]",
+                badID != null ?
+                        String.format("\"%s\"", badID)
+                        : badID));
     }
 }
